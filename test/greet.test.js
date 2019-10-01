a2b37d4c6d0100bc0b4729976460f133c8ab74dc
Odwa Makhalima <3652687@myuwc.ac.za>
2019-10-01 16:23:08 +0200

    first

---
+++ test/greet.test.js
@@ -0,0 +1,43 @@
+let assert = require("assert");
+var greetings = require('../greetFact')
+
+describe('Greeting-App function', function () {
+
+    it('should return the list of all the names greeted', function () {
+
+        var setFact = greetings()
+
+        setFact.storedNames('odwa')
+        setFact.storedNames('jesse')
+        setFact.storedNames('jason')
+
+        assert.deepEqual([ { allNames: 'ODWA' },
+        { allNames: 'JESSE' },
+        { allNames: 'JASON' } ], setFact.nameList());
+    });
+
+
+    it('should return the number of names and ignore duplicates', function () {
+
+        var setFact = greetings()
+
+        setFact.storedNames('odwa')
+        setFact.storedNames('odwa')
+        setFact.storedNames('jesse')
+        setFact.storedNames('jesse')
+
+        assert.deepEqual(2, setFact.count());
+    });
+
+    it('should greet the name in xhosa,english and afrikaans', function () {
+
+        var setFact = greetings()
+
+        setFact.storedNames('odwa')
+        
+
+        assert.deepEqual('Molo ODWA', setFact.greetName('Xhosa'));
+        assert.deepEqual('Hello ODWA', setFact.greetName('English'));
+        assert.deepEqual('Hallo ODWA', setFact.greetName('Afrikaans'));
+    });
+});
